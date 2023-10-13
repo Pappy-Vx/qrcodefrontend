8e84c82036e15fc2a2e5244a57d8d8e0d0850ae1
kolapo <pappy.kay9@gmail.com>
2023-10-13 02:55:30 +0100

    added responsiveness

--- script.js
+++ script.js
@@ -2,19 +2,17 @@ const updateMovies = async () => {
     try {
         const response = await fetch('https://qrcode-cz3n.onrender.com/qr/data');
         const data = await response.json();
-        const movies = data.movies; // Assuming 'movies' is an array of movies
+        const movies = data.movies; 
 
-        // Randomly select 10 movies
         const selectedMovies = getRandomMovies(movies, 20);
 
-
-        // Update the movie data properties for 10 div elements
+       
         for (let i = 1; i <= 10; i++) {
             const movieElement = document.getElementById(`movie${i}`);
             movieElement.innerHTML = `<img src=${selectedMovies[i-1].Images[0]} >`
             movieElement.innerHTML += `<h2>${selectedMovies[i - 1].Title}</h2>`;
             movieElement.innerHTML += `<p>Year: ${selectedMovies[i - 1].Year}</p>`;
-            // Add more properties as needed
+            
         }
 
     } catch (error) {
@@ -22,13 +20,13 @@ const updateMovies = async () => {
     }
 };
 
-// Initial update
+
 updateMovies();
 
-// Periodically update the QR code and movie data every 10 seconds
+
 setInterval(updateMovies, 10000);
 
-// Function to randomly select 'n' items from an array
+
 function getRandomMovies(array, n) {
     const shuffled = array.sort(() => 0.5 - Math.random());
     return shuffled.slice(0, n);
